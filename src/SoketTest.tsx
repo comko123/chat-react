67b4d488f22884c901a68b451e417c49d332474f
Minseok Kim <comko123@naver.com>
2022-10-27 15:26:30 +0900

    alone code

--- src/SoketTest.tsx
+++ src/SoketTest.tsx
@@ -1,16 +1,17 @@
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import SockJS from 'sockjs-client'
-
 
 const sock = new SockJS('https://chat-springmvc.herokuapp.com/chat')
 
 const SoketTest = () => {
-    const out = useRef<HTMLDivElement>(null)
+    const navigate =  useNavigate()
     const msg = useRef<HTMLInputElement>(null)
-
+const [chat,setChat] = useState<(string|undefined)[]>([])
     const sendMessage = () =>{
         if(!msg.current?.value)return;
         sock.send(msg.current?.value)
+        setChat(state=>[...state,msg.current?.value])
         return msg.current?.value === null
     }
     const onMessage = (msg:any) => {
@@ -31,25 +32,28 @@ const SoketTest = () => {
         else{return <p>{sessionId+":"+message}</p>}}
     
     const onClose = () => {
-     return <div>user님이 퇴장하셨습니다.</div>
+        setChat(chat=>[...chat,"user님이 퇴장하셨습니다."])   
     }
     
     const onOpen = () => {
-        if(!out.current)return;
-
-        return out.current.textContent = "user님이 입장하셨습니다."
+        setChat(chat=>["user님이 입장하셨습니다.",...chat])   
     }
     sock.onmessage = onMessage
     sock.onclose = onClose
     sock.onopen = onOpen
+
+    useEffect(()=>onOpen(),[])
     return(<><h1>chat</h1>
-	<hr/><></>
-	<div><div id="msgArea" ></div><div>
-            {onOpen()}
-            <div ref={out}></div>
+	<hr/>
+	<div><div id="msgArea" >
+        {chat.map((item,index) =><div key={index}>{item}</div>)}
+        </div><div>
             <form onSubmit={e=>e.preventDefault()}>
 			message : <input type="text" id="msg" ref = {msg}/>
 			<input type = "button" onClick={sendMessage} value = "보내기"/>
-            <input type = "button" onClick={onClose} value = "나가기"/>
+            <input type = "button" onClick={()=>{
+                onClose()
+                navigate('/')
+            }} value = "나가기"/>
     </form></div></div></>)}
 export default SoketTest
