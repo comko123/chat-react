040b6b780879a444c2991b72264732ae511368e8
Minseok Kim <comko123@naver.com>
2022-12-24 00:02:33 +0900

    .

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,11 +1,9 @@
 import { useNavigate } from "react-router-dom"
 
 const HomePage = () => {
-  const router = useNavigate()
+const router = useNavigate()
 return(<>
 <h3>start live chat</h3>
-<button onClick = {()=>router('/soket')}>Soket Chat</button>
-</>)
-} 
+<button onClick = {()=>router('/soket')}>Soket Chat</button></>)} 
 export default HomePage
 //https://velog.io/@cksal5911/WebSoket-stompJSReact-%EC%B1%84%ED%8C%85-1
